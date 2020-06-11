3157155827ea3606f78d2545b1aa81a033b041cb
Dexter <dexterhuang4577@gmail.com>
2020-06-11 19:03:02 +0800

    Finished project

--- public/javascript/fileUpload.js
+++ public/javascript/fileUpload.js
@@ -1,15 +1,22 @@
-FilePond.registerPlugin(
+if (document.readyState == 'loading') {
+  document.getElementById('main-css').addEventListener('load', ready)
+} else {
+  ready()
+}
+
+function ready() {
+  FilePond.registerPlugin(
     FilePondPluginImagePreview,
     FilePondPluginImageResize,
     FilePondPluginFileEncode
-)
+  )
 
-FilePond.setOptions({
-  // set ratio according to "cover" input field (_form_field)
-  stylePanelAspectRatio: 150 / 100,
-  imageResizeTargetWidth: 100,
-  imageResizeTargetHeight: 150
-})
+  FilePond.setOptions({
+    // set ratio according to "cover" input field (_form_field)
+    stylePanelAspectRatio: 150 / 100,
+    imageResizeTargetWidth: 100,
+    imageResizeTargetHeight: 150
+  })
 
-  
-FilePond.parse(document.body)
+  FilePond.parse(document.body)
+}
